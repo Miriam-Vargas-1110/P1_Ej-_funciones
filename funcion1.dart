3ea9c9331a1e98fecaad6d66ab25fcafe1a2567c
Miriam-Vargas-1110 <164534211+Miriam-Vargas-1110@users.noreply.github.com>
2025-02-07 16:48:37 +0000

    Mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,20 @@
+//funcion que no devuelve valor
+//funcion saludar
+void saludar (){
+    print ('Hola mundo');
+}
+
+void escribirnombre(){
+  print('Miriam Vargas ');
+}
+
+void main (){
+   //Miriam Vargas Carrillo:22308051281110
+   //Llamar a la funcion saludar
+   print('Miriam Vargas Carrillo:22308051281110');
+   print('Llamando a la funcion saludar');
+   saludar();
+   print('Llamando a la funcion escribirnombre');
+   escribirnombre();
+ 
+}
